d55c2683a4658742f93114cefe0a8dd383f771e1
Rahul077177 <rahul@moneynmore.com>
2024-03-19 12:41:43 +0530

    added fix

--- lib/data/network/network_service.dart
+++ lib/data/network/network_service.dart
@@ -1,62 +1,9 @@
-// import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class NetworkService {
-  // final Dio dio = Dio();
-  final String baseUrl;
-  // final CustomInterceptors interceptors = CustomInterceptors();
-
   NetworkService({required this.baseUrl});
-  // Future<Response> get(String path,
-  //     {Map<String, dynamic>? queryParameters}) async {
-  //   try {
-  //     final response = await dio.get(
-  //       baseUrl + path,
-  //       queryParameters: queryParameters,
-  //     );
-  //     return response;
-  //   } catch (e) {
-  //     print('the error is $e');
-  //     throw Exception();
-  //   }
-  // }
+  final String baseUrl;
 
-  // Future<Response> post(String path,
-  //     {required Map<String, dynamic> data}) async {
-  //   try {
-  //     final response = await dio.post(
-  //       baseUrl + path,
-  //       data: data,
-  //     );
-  //     return response;
-  //   } catch (e) {
-  //     throw Exception();
-  //   }
-  // }
-
-  // Future<Response> delete(String path, {Map<String, dynamic>? data}) async {
-  //   try {
-  //     final response = await dio.delete(
-  //       baseUrl + path,
-  //       data: data,
-  //     );
-  //     return response;
-  //   } catch (e) {
-  //     throw Exception();
-  //   }
-  // }
-
-  // Future<Response> update(String path, Map<String, dynamic> data) async {
-  //   try {
-  //     final response = await dio.put(
-  //       baseUrl + path,
-  //       data: data,
-  //     );
-  //     return response;
-  //   } catch (e) {
-  //     throw Exception();
-  //   }
-  // }
   Future<http.Response> get(String path,
       {Map<String, dynamic>? queryParameters}) async {
     final http.Response response = await http.get(Uri.parse(baseUrl + path));
